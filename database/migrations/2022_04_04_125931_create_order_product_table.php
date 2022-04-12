d33edf555e4d3f546f0243f2b559886722bb3e27
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2022-04-12 15:41:21 +0700

    Create Database
    - Create Table/ Model

---
+++ database/migrations/2022_04_04_125931_create_order_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_order');
+            $table->unsignedBigInteger('id_product');
+            $table->unsignedInteger('amount');
+            $table->unsignedBigInteger('total_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
---
+++ database/migrations/2022_04_04_130100_create_review_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_product');
+            $table->tinyInteger('star_rating');
+            $table->string('comment')->nullable();
+            $table->timestamp('time')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+};
