90db8bea450e0851aaf614547712d72f86e14459
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2022-04-18 20:17:33 +0700

    update db 9

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,40 +20,42 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // User::insert([
-        //     'name' => 'PTH',
-        //     'address' => 'ĐN',
-        //     'phone' => '01234567890',
-        //     'email' => 'thanhhoi@gmail.com',
-        //     'role' => 0,
-        //     'password' => Hash::make(123456),
-        //     'avatar' => '',
-        //     'email_verified_at' => now(),
-        //     'remember_token' => '1234567890',
-        // ]);                
+        User::insert([
+            'name' => 'PTH',
+            'address' => 'ĐN',
+            'phone' => '01234567890',
+            'email' => 'thanhhoi@gmail.com',
+            'role' => 0,
+            'password' => Hash::make(123456),
+            'avatar' => '',
+            'email_verified_at' => now(),
+            'remember_token' => '1234567890',
+        ]);                
         User::factory(3)->create(
             ['role' => 2]
         );
-        User::factory(20)->create()->each(function($user){
-            $product = Product::all();
-            Order::factory(rand(1,10))->create([
-                'id_user' => $user->id
-            ])->each(function($order) use ($product, $user){
-                $array_id = $product->random(rand(1,$product->count()))->pluck('id')->toArray();
-                $total_price = 0;
-                foreach($array_id as $id){
-                    $total_price += Product::where('id',$id)->first()->price;
-                    Review::factory(rand(0,1))->create([
-                        'id_user' => $user->id,
-                        'id_product' => $id
-                    ]);
-                }
-                $order->product()->attach(
-                    $array_id,
-                    ['amount' => rand(1,10),
-                    'total_price' => $total_price],
-                );
-            });
-        });
+        
+        User::factory(16)->create();
+        // User::factory(20)->create()->each(function($user){
+        //     $product = Product::all();
+        //     Order::factory(rand(1,10))->create([
+        //         'id_user' => $user->id
+        //     ])->each(function($order) use ($product, $user){
+        //         $array_id = $product->random(rand(1,$product->count()))->pluck('id')->toArray();
+        //         $total_price = 0;
+        //         foreach($array_id as $id){
+        //             $total_price += Product::where('id',$id)->first()->price;
+        //             Review::factory(rand(0,1))->create([
+        //                 'id_user' => $user->id,
+        //                 'id_product' => $id
+        //             ]);
+        //         }
+        //         $order->product()->attach(
+        //             $array_id,
+        //             ['amount' => rand(1,10),
+        //             'total_price' => $total_price],
+        //         );
+        //     });
+        // });
     }
 }
