5e8153a3059831d0648786fffc255886c59bab88
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2022-04-20 15:03:34 +0700

    api login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,5 +15,13 @@ class UserController extends Controller
             echo $data->name;
             echo '<br>';
         }
+    }
+
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(!$user || Hash::check($request->password, $user->password)){
+            return ["error" => "Incorrect email or password"];
+        }
+        return $user;
     }
 }
