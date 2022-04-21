866155b594a5ea776ee7f434daecee2620647dcd
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2022-04-21 10:05:06 +0700

    GetAllProduct

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('v1/login',[UserController::class,'login']);
 Route::post('v1/register',[UserController::class,'register']);
+Route::get('v1/getAllProduct',[ProductController::class,'getAllProduct']);
